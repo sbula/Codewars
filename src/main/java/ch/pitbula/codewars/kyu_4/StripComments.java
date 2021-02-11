b5873c64fd29b304527c5f415e23456305651421
Steve Bula <steve.bula@gmail.com>
2021-02-11 15:53:16 +0100

    add 4kyu challenge StripComments

--- src/main/java/ch/pitbula/codewars/kyu_4/StripComments.java
+++ src/main/java/ch/pitbula/codewars/kyu_4/StripComments.java
@@ -4,11 +4,24 @@
 
 package ch.pitbula.codewars.kyu_4;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class StripComments {
 
     public static String stripComments(String text, String[] commentSymbols) {
-
-        return "";
+        return Arrays.stream(text.split("\n"))
+                .map(line -> removeComment(line, commentSymbols))
+                .map(String::stripTrailing)
+                .collect(Collectors.joining("\n"));
     }
 
+    private static String removeComment(String line, String[] symbols) {
+        for (String cmdSymbol : symbols) {
+            int index = line.indexOf(cmdSymbol);
+            line = index<0 ? line : line.substring(0, index);
+        }
+
+        return line;
+    }
 }
--- src/test/java/ch/pitpula/codewars/kyu_4/StripCommentsTest.java
+++ src/test/java/ch/pitpula/codewars/kyu_4/StripCommentsTest.java
@@ -5,17 +5,42 @@
 package ch.pitpula.codewars.kyu_4;
 
 
+import ch.pitbula.codewars.kyu_4.StripComments;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class StripCommentsTest {
 
-//    @Test
-//    public void stripComments() {
-//        assertEquals("apples, pears\ngrapes\nbananas",
-//                StripComments.stripComments( "apples, pears # and bananas\ngrapes\nbananas !apples", new String[] { "#", "!" } )
-//        );
-//
-//        assertEquals("a\nc\nd",
-//                StripComments.stripComments( "a #b\nc\nd $e f g", new String[] { "#", "$" } )
-//        );
-//
-//    }
+    @Test
+    public void stripComments_same() {
+        final String text = "apples, pears\ngrapes\nbananas";
+        assertEquals(text, StripComments.stripComments(text, new String[] { "4", "5" }));
+    }
+
+    @Test
+    public void stripComments_whiteSpaces() {
+        final String input = "apples, pears          \n   grapes \nbananas";
+        final String expected = "apples, pears\n   grapes\nbananas";
+        assertEquals(expected, StripComments.stripComments(input, new String[] { "4", "5" }));
+    }
+
+    @Test
+    public void stripComments_oneMarker() {
+        final String input = "apples, pears # and bananas\n  grapes  \nbananas !apples";
+        final String expected = "apples, pears\n  grapes\nbananas !apples";
+        assertEquals(expected, StripComments.stripComments(input, new String[] { "#" }));
+    }
+
+    @Test
+    public void stripComments() {
+        assertEquals("apples, pears\ngrapes\nbananas",
+                StripComments.stripComments( "apples, pears # and bananas\ngrapes\nbananas !apples", new String[] { "#", "!" } )
+        );
+
+        assertEquals("a\nc\nd",
+                StripComments.stripComments( "a #b\nc\nd $e f g", new String[] { "#", "$" } )
+        );
+
+    }
 }
